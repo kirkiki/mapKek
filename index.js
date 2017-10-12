7f7a4e12a676b0f5641bf2f707d31744a713b670
Ronan <ronanpsf@gmail.com>
2017-10-12 17:47:17 +0200

    commit map 2

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ var Case = /** @class */ (function () {
     return Case;
 }());
 var case1 = new Case(20, 20);
-draw(10, 10);
-function draw(x, y) {
+init(10, 10);
+function init(x, y) {
     var canvas = document.getElementById('mon_canvas');
     var context = canvas.getContext("2d");
     for (var i = 0; i < y; i += 1) {
         for (var j = 0; j < x; j += 1) {
-            context.beginPath();
-            context.rect(j * 20, i * 20, 20, 20);
-            context.stroke();
+            drawCase(new Case(i, j));
         }
     }
     context.beginPath();
     context.arc(10, 10, 10, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+}
+function drawCase(newCase) {
+    var canvas = document.getElementById('mon_canvas');
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
     context.stroke();
 }
