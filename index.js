8d1127cf9ddc7cb6bdda0f5984e343f59d1b3744
Aria Groult <aria.groult@outlook.fr>
2017-10-12 18:22:06 +0200

    added delete case

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var Case = /** @class */ (function () {
     }
     return Case;
 }());
-var case1 = new Case(20, 20);
-init(10, 10);
+var Player = /** @class */ (function () {
+    function Player(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Player.prototype.setPosition = function (x, y) {
+        this.x = x;
+        this.y = y;
+    };
+    return Player;
+}());
+;
 function init(x, y) {
-    var canvas = document.getElementById('mon_canvas');
-    var context = canvas.getContext("2d");
     for (var i = 0; i < y; i += 1) {
         for (var j = 0; j < x; j += 1) {
             drawCase(new Case(i, j));
@@ -27,3 +35,14 @@ function drawCase(newCase) {
     context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
     context.stroke();
 }
+function clearCase(block) {
+    context.beginPath();
+    context.rect(block.x, block.y, 20, 20);
+    context.fillStyle = "white";
+    context.fill();
+    drawCase(block);
+}
+var canvas = document.getElementById('mon_canvas');
+var context = canvas.getContext("2d");
+var case1 = new Case(20, 20);
+init(10, 10);
--- index.ts
+++ index.ts
@@ -1,33 +1,54 @@
 class Case {
-    x : number;
-    y : number;
-    constructor(x,y) {
-        this.x = x; 
+    x: number;
+    y: number;
+    constructor(x, y) {
+        this.x = x;
         this.y = y;
     }
 }
 
-var case1 = new Case(20,20);
-init(10,10);
+class Player {
+    x: number;
+    y: number;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+};
 
-function init(x,y){
-    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
-    let context = canvas.getContext("2d");
-    for(let i : number = 0; i<y; i+=1){        
-        for(let j : number = 0; j<x; j+=1){       
-            drawCase(new Case(i,j));
+function init(x, y) {
+    for (let i: number = 0; i < y; i += 1) {
+        for (let j: number = 0; j < x; j += 1) {
+            drawCase(new Case(i, j));
         }
     }
-    context.beginPath();               
-    context.arc(10,10,10,0, Math.PI*2);
+    context.beginPath();
+    context.arc(10, 10, 10, 0, Math.PI * 2);
     context.fill();
-    context.stroke();     
-}
-
-function drawCase(newCase){
-    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
-    let context = canvas.getContext("2d");
-    context.beginPath();               
-    context.rect(newCase.x*20,newCase.y*20,20,20);
     context.stroke();
 }
+
+function drawCase(newCase) {
+    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
+    context.stroke();
+}
+
+function clearCase(block) {
+    context.beginPath();
+    context.rect(block.x, block.y, 20, 20);
+    context.fillStyle = "white";
+    context.fill();
+    drawCase(block);
+}
+
+let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
+let context = canvas.getContext("2d");
+var case1 = new Case(20, 20);
+init(10, 10);
