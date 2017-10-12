46d0e10a43103559cb88e9d4e90e86be0bc71b16
Aria Groult <aria.groult@outlook.fr>
2017-10-12 17:27:40 +0200

    added player

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("ok");
+window.onload = function () {
+    var name = "Sexy Player";
+    //Instantiation du joueur et de l'ordinateur
+    var player = new Player(name);
+    var uiPlayer = new Player('UI');
+    var xPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var yPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var uiPlayerX = xPositions[Math.floor(Math.random() * xPositions.length)];
+    var uiPlayerY = yPositions[Math.floor(Math.random() * yPositions.length)];
+    //positionnement des joueurs
+    player.position(0, 0);
+    uiPlayer.position(uiPlayerX, uiPlayerY);
+};
+var Player = /** @class */ (function () {
+    function Player(name) {
+        this.name = name;
+    }
+    Player.prototype.position = function (x, y) {
+        this.x = x;
+        this.y = y;
+    };
+    return Player;
+}());
+;
--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log("ok")
+window.onload = function () {
+    let name = "Sexy Player";
+    //Instantiation du joueur et de l'ordinateur
+    let player = new Player(name);
+    let uiPlayer = new Player('UI');
+    let xPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let yPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let uiPlayerX = xPositions[Math.floor(Math.random() * xPositions.length)];
+    let uiPlayerY = yPositions[Math.floor(Math.random() * yPositions.length)];
+
+    //positionnement des joueurs
+    player.position(0, 0);
+    uiPlayer.position(uiPlayerX, uiPlayerY);
+
+
+};
+
+
+
+class Player {
+    name: string;
+    x: number;
+    y: number;
+    template: string;
+    constructor(name) {
+        this.name = name;
+    }
+    position(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+};
