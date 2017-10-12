387fa4d4b07a937565e77de3f6b39341601da680
Ronan <ronanpsf@gmail.com>
2017-10-12 18:10:23 +0200

    commit rand

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var canvas = document.getElementById('mon_canvas');
+var context = canvas.getContext("2d");
+var xMap = 10;
+var yMap = 10;
 var Case = /** @class */ (function () {
     function Case(x, y) {
         this.x = x;
@@ -6,24 +10,33 @@ var Case = /** @class */ (function () {
     return Case;
 }());
 var case1 = new Case(20, 20);
-init(10, 10);
+init(xMap, yMap);
 function init(x, y) {
-    var canvas = document.getElementById('mon_canvas');
-    var context = canvas.getContext("2d");
     for (var i = 0; i < y; i += 1) {
         for (var j = 0; j < x; j += 1) {
             drawCase(new Case(i, j));
         }
     }
+    shuffle(10);
     context.beginPath();
     context.arc(10, 10, 10, 0, Math.PI * 2);
     context.fill();
     context.stroke();
 }
 function drawCase(newCase) {
-    var canvas = document.getElementById('mon_canvas');
-    var context = canvas.getContext("2d");
     context.beginPath();
     context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
     context.stroke();
+}
+function shuffle(nbWall) {
+    while (nbWall > 0) {
+        var xRand = Math.floor((Math.random() * (xMap)) + 1) * 20;
+        var yRand = Math.floor((Math.random() * (yMap)) + 1) * 20;
+        context.beginPath();
+        context.rect(xRand, xRand, 20, 20);
+        context.fillStyle = "blue";
+        context.fillRect;
+        context.stroke();
+        nbWall--;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+var canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
+var context = canvas.getContext("2d");
+var xMap = 10;
+var yMap = 10;
 class Case {
     x : number;
     y : number;
@@ -8,16 +12,15 @@ class Case {
 }
 
 var case1 = new Case(20,20);
-init(10,10);
+init(xMap,yMap);
 
 function init(x,y){
-    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
-    let context = canvas.getContext("2d");
     for(let i : number = 0; i<y; i+=1){        
         for(let j : number = 0; j<x; j+=1){       
             drawCase(new Case(i,j));
         }
     }
+    shuffle(10);
     context.beginPath();               
     context.arc(10,10,10,0, Math.PI*2);
     context.fill();
@@ -25,9 +28,20 @@ function init(x,y){
 }
 
 function drawCase(newCase){
-    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
-    let context = canvas.getContext("2d");
     context.beginPath();               
     context.rect(newCase.x*20,newCase.y*20,20,20);
     context.stroke();
+}
+
+function shuffle(nbWall){
+    while(nbWall> 0){
+        let xRand : number = Math.floor((Math.random() * (xMap)) + 1) * 20; 
+        let yRand : number = Math.floor((Math.random() * (yMap)) + 1) * 20; 
+        context.beginPath();               
+        context.rect(xRand,xRand,20,20);
+        context.fillStyle= "blue";
+        context.fillRect;
+        context.stroke();
+        nbWall --;
+    }
 }
