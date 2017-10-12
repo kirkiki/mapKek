559600eeaef1899054d686acda6ccb3bcc41dc06
Ronan <ronanpsf@gmail.com>
2017-10-12 17:25:57 +0200

    commit map

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("ok");
+var Case = /** @class */ (function () {
+    function Case(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Case;
+}());
+var case1 = new Case(20, 20);
+draw(10, 10);
+function draw(x, y) {
+    var canvas = document.getElementById('mon_canvas');
+    var context = canvas.getContext("2d");
+    for (var i = 0; i < y; i += 1) {
+        for (var j = 0; j < x; j += 1) {
+            context.beginPath();
+            context.rect(j * 20, i * 20, 20, 20);
+            context.stroke();
+        }
+    }
+    context.beginPath();
+    context.arc(10, 10, 10, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("ok")
+class Case {
+    x : number;
+    y : number;
+    constructor(x,y) {
+        this.x = x; 
+        this.y = y;
+
+    }
+}
+
+var case1 = new Case(20,20)
+draw(10,10)
+
+function draw(x,y){
+    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
+    let context = canvas.getContext("2d");
+    for(let i : number = 0; i<y; i+=1){        
+        for(let j : number = 0; j<x; j+=1){       
+            context.beginPath();               
+            context.rect(j*20,i*20,20,20)   
+            context.stroke();     
+        }
+    }
+    context.beginPath();               
+    context.arc(10,10,10,0, Math.PI*2)
+    context.fill()
+    context.stroke();     
+}
