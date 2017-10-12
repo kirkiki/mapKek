aa9b9bbd27398c228fcc12371f3780b946d18ca9
Aria Groult <aria.groult@outlook.fr>
2017-10-12 19:38:49 +0200

    did movement

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var canvas = document.getElementById('mon_canvas');
 var context = canvas.getContext("2d");
 var xMap = 10;
 var yMap = 10;
+var player;
 var Case = /** @class */ (function () {
     function Case(x, y) {
         this.x = x;
         this.y = y;
+        context.beginPath();
+        context.rect(this.x * 20, this.y * 20, 20, 20);
+        context.stroke();
     }
     return Case;
 }());
@@ -13,40 +17,41 @@ var Player = /** @class */ (function () {
     function Player(x, y) {
         this.x = x;
         this.y = y;
+        context.beginPath();
+        context.arc(x, y, 10, 0, Math.PI * 2);
+        context.fillStyle = "black";
+        context.fill();
+        context.stroke();
     }
     Player.prototype.setPosition = function (x, y) {
         this.x = x;
         this.y = y;
     };
+    Player.prototype.getPlayerX = function () {
+        return this.x;
+    };
+    Player.prototype.getPlayerY = function () {
+        return this.y;
+    };
     return Player;
 }());
 ;
+function clearCase(x, y, callback) {
+    context.beginPath();
+    context.rect(x, y, 20, 20);
+    context.fillStyle = "white";
+    context.fill();
+    new Case(x, y);
+    callback();
+}
 function init(x, y) {
     for (var i = 0; i < y; i += 1) {
         for (var j = 0; j < x; j += 1) {
-            drawCase(new Case(i, j));
+            new Case(i, j);
         }
     }
+    player = new Player(10, 10);
     shuffle(10);
-    context.beginPath();
-    context.arc(10, 10, 10, 0, Math.PI * 2);
-    context.fillStyle = "black";
-    context.fill();
-    context.stroke();
-}
-function drawCase(newCase) {
-    var canvas = document.getElementById('mon_canvas');
-    var context = canvas.getContext("2d");
-    context.beginPath();
-    context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
-    context.stroke();
-}
-function clearCase(block) {
-    context.beginPath();
-    context.rect(block.x, block.y, 20, 20);
-    context.fillStyle = "white";
-    context.fill();
-    drawCase(block);
 }
 function shuffle(nbWall) {
     while (nbWall > 0) {
@@ -59,5 +64,21 @@ function shuffle(nbWall) {
         nbWall--;
     }
 }
+document.body.onkeyup = function (e) {
+    switch (e.which) {
+        case 39:
+            player = new Player(player.x + 20, player.y);
+            break;
+        case 37:
+            player = new Player(player.x - 20, player.y);
+            break;
+        case 38:
+            player = new Player(player.x, player.y - 20);
+            break;
+        case 40:
+            player = new Player(player.x, player.y + 20);
+            break;
+    }
+};
 var case1 = new Case(20, 20);
 init(10, 10);
--- index.ts
+++ index.ts
@@ -2,12 +2,16 @@ var canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
 var context = canvas.getContext("2d");
 var xMap = 10;
 var yMap = 10;
+var player;
 class Case {
     x: number;
     y: number;
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        context.beginPath();
+        context.rect(this.x * 20, this.y * 20, 20, 20);
+        context.stroke();
     }
 }
 
@@ -17,53 +21,71 @@ class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        context.beginPath();
+        context.arc(x, y, 10, 0, Math.PI * 2);
+        context.fillStyle = "black";
+        context.fill();
+        context.stroke();
     }
     setPosition(x, y) {
         this.x = x;
         this.y = y;
     }
+    getPlayerX() {
+        return this.x;
+    }
+    getPlayerY() {
+        return this.y;
+    }
 };
+
+function clearCase(x, y, callback) {
+    context.beginPath();
+    context.rect(x, y, 20, 20);
+    context.fillStyle = "white";
+    context.fill();
+    new Case(x, y);
+    callback();
+}
+
 function init(x, y) {
     for (let i: number = 0; i < y; i += 1) {
         for (let j: number = 0; j < x; j += 1) {
-            drawCase(new Case(i, j));
+            new Case(i, j);
         }
     }
+    player = new Player(10, 10);
     shuffle(10)
-    context.beginPath();
-    context.arc(10, 10, 10, 0, Math.PI * 2);
-    context.fillStyle = "black";
-    context.fill();
-    context.stroke();
+
 }
 
-function drawCase(newCase) {
-    let canvas = <HTMLCanvasElement>document.getElementById('mon_canvas');
-    let context = canvas.getContext("2d");
-    context.beginPath();
-    context.rect(newCase.x * 20, newCase.y * 20, 20, 20);
-    context.stroke();
-}
-
-function clearCase(block) {
-    context.beginPath();
-    context.rect(block.x, block.y, 20, 20);
-    context.fillStyle = "white";
-    context.fill();
-    drawCase(block);
-}
-function shuffle(nbWall){
-    while(nbWall>0){
-        let xRand : number = Math.floor((Math.random() * (xMap))) * 20; 
-        let yRand : number = Math.floor((Math.random() * (yMap))) * 20; 
-        context.beginPath(); 
+function shuffle(nbWall) {
+    while (nbWall > 0) {
+        let xRand: number = Math.floor((Math.random() * (xMap))) * 20;
+        let yRand: number = Math.floor((Math.random() * (yMap))) * 20;
+        context.beginPath();
         context.fillStyle = "blue";
-        context.fillRect(xRand,yRand,20,20);
+        context.fillRect(xRand, yRand, 20, 20);
         context.stroke();
         nbWall--;
     }
 }
 
-
+document.body.onkeyup = function (e) {
+    switch (e.which) {
+        case 39:
+            player = new Player(player.x + 20, player.y);
+            break;
+        case 37:
+            player = new Player(player.x - 20, player.y);
+            break;
+        case 38:
+            player = new Player(player.x, player.y - 20);
+            break;
+        case 40:
+            player = new Player(player.x, player.y + 20);
+            break;
+    }
+};
 var case1 = new Case(20, 20);
 init(10, 10);
